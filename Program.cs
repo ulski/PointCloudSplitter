f773aa6ef4b06a428fbc89461ba68625b8176134
ulski_cp <SND\ulski_cp>
2012-01-23 01:21:38 +0000

    split to temp cubefiles (cubes missing point count header line) performance improved 3.66gb pts split to 1025cubes in 8minutes

--- Program.cs
+++ Program.cs
@@ -36,6 +36,35 @@ namespace PointCloudSplitter
 
     class Program
     {
+        public static int DictionaryBufferSize = 4000000;
+        /*
+         * testing pts file 3,66 GB (3 937 108 560 bytes)
+         * Time elapsed: 00:37:20.2635846 buffer size 1000
+         * output only 2.37gb? in first run
+         * 
+         * second run with buffer size 2000000
+         * also changed disk write from string to stringbuilder
+         * and also added a stream flush command
+         * Time elapsed: 00:08:35.2906564
+         * 3.42gb now written to disk
+         * 
+         * third run
+         * changed stingbuilder obj to one stream writeline per point
+         * 3.66gb written to disk
+         * uses less memory and approx. same speed
+         * Time elapsed: 00:08:39.4959986
+         * 
+         * 4th test
+         * buffer 4000000, peak working set 1005076K
+         * Time elapsed: 00:08:20.8302115
+         * 3,66 GB (3 937 108 540 bytes) written (20bytes diff. could be due to missing header
+         * from orgfile (temp pts files are headerless) 
+         * 
+         * 5th test
+         * buffer 4000000, peak working set 987352K
+         * Time elapsed: 00:07:56.9039535
+         * written :3,66 GB (3 937 108 540 bytes)
+         */
         public static List<string> GetFiles(string path, List<string> searchPatterns, SearchOption searchOption)
         {
             var result = searchPatterns.SelectMany(x => Directory.GetFiles(path, x, searchOption));
@@ -93,9 +122,9 @@ namespace PointCloudSplitter
                 {
                     Console.WriteLine("Error, invalid scan date:" + a.ScanDate);
                     return;
-                }    
+                }
             }
-            
+
             //load the config file
             KeyValueConfigurationCollection LoadedAppSettings = UsingConfigurationManager.GetAppConfiguration(a.ConfigFile);
 
@@ -218,7 +247,7 @@ namespace PointCloudSplitter
                 }
             }
             else
-            { 
+            {
                 //set default syntax
                 Console.WriteLine(@"Error in ptssyntax configuration, using default value ""p x y z i r g b""");
                 ptssyntax = new List<string>();
@@ -268,18 +297,18 @@ namespace PointCloudSplitter
                 cubefilenamesyntax.Add("DD");
                 cubefilenamesyntax.Add("¤.pts¤");
             }
-            
+
             #endregion
 
-            
-            
+
+
             #region findptsfiles
             // search all subfolders, perhaps this should be a setting
             SearchOption mysearchOption = SearchOption.AllDirectories;
-            
+
             List<string> mysearchPatterns = new List<string>();
             mysearchPatterns.Add("*.pts");
-            
+
             List<string> ptsfiles = GetFiles(SourceFolder, mysearchPatterns, mysearchOption);
             decimal NoOfInputFiles = ptsfiles.Count;
             if (NoOfInputFiles == 0)
@@ -301,111 +330,251 @@ namespace PointCloudSplitter
             /* dictionary holding counts per cube.
              * each cube needs a point count in the header*/
             Dictionary<string, ulong> CubeDictionary = new Dictionary<string, ulong>();
-            
+            Dictionary<string, List<string>> CubeBufferDictionary = new Dictionary<string, List<string>>();
+            int PointsInMemory = 0;
+            int TotalPointCounter = 0;
+            string MyCubeFullPath = string.Empty;
+            string MyDictionaryKey = string.Empty;
             foreach (string FileName in ptsfiles)
             {
+                #region processptsfile
+                int TotalFilePointCounter = 0;
                 if (File.Exists(FileName))
                 {
                     Console.WriteLine("Reading:" + FileName);
-                    FileStream Input = new FileStream(FileName, FileMode.Open, FileAccess.Read);
-                    StreamReader SR = new StreamReader(Input);
+                    FileStream MyInputFileStream = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                    StreamReader MyInputStreamReader = new StreamReader(MyInputFileStream);
                     string[] Current;
-                    int TotalPointCounter = 0;
-                    
-                    string Str = SR.ReadLine();
+
+                    string Str = string.Empty;
+                    try
+                    {
+                        Str = MyInputStreamReader.ReadLine();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                        return;
+                    }
+
                     while (Str != null)
                     {
-                        Current = Str.Split(SeparatorCharArray, StringSplitOptions.RemoveEmptyEntries);
-                        if (Current.Length==1)
+                        //flush buffer when it gets full
+                        if (PointsInMemory >= DictionaryBufferSize)
                         {
-                            Console.WriteLine("Number of scan points announced in file:" + Current[0]);    
+                            string MyFlushError = string.Empty;
+                            if (FlushBufferToDisk(DestinationFolder, CubeBufferDictionary, ref PointsInMemory, out MyFlushError) == false)
+                            {
+                                Console.WriteLine("Error," + MyFlushError);
+                                return;
+                            }
                         }
+                        Current = Str.Split(SeparatorCharArray, StringSplitOptions.RemoveEmptyEntries);
+                        if (Current.Length == 1)
+                        {
+                            Console.WriteLine("Number of scan points announced in file:" + Current[0]);
+                        }
+                        #region processpoint
                         if (Current.Length > 1)
                         {
                             //Console.WriteLine("Col0:" + Current[0] + " Col1:" + Current[1] + " Col2:" + Current[2]);
-                            
+
                             resx = decimal.TryParse(Current[xcolumn], style, culture, out CoordX);
                             resy = decimal.TryParse(Current[ycolumn], style, culture, out CoordY);
                             resz = decimal.TryParse(Current[zcolumn], style, culture, out CoordZ);
                             //only increase the counter by one if we got a vadid point
-                            if (resx==true && resy==true && resz==true)
+                            if (resx == true && resy == true && resz == true)
                             {
                                 CubeNoX = Convert.ToInt32(Math.Ceiling(CoordX / CubeLengthX));
                                 CubeNoY = Convert.ToInt32(Math.Ceiling(CoordY / CubeLengthY));
                                 CubeNoZ = Convert.ToInt32(Math.Ceiling(CoordZ / CubeLengthZ));
                                 //Console.WriteLine("Debug this point belongs in cube : [" + CubeNoX + " " + CubeNoY + " " + CubeNoZ + "]");
-                                string MyDictionaryKey=(CubeNoX + " " + CubeNoY + " " + CubeNoZ);
-                                string MyCubeFullPath = DestinationFolder +@"\" +MyDictionaryKey+".ptstemp";
+                                MyDictionaryKey = (CubeNoX + " " + CubeNoY + " " + CubeNoZ);
+                                MyCubeFullPath = DestinationFolder + @"\" + MyDictionaryKey + ".ptstemp";
+                                bool newcube = false;
                                 if (!CubeDictionary.ContainsKey(MyDictionaryKey))
                                 {
-                                    CubeDictionary.Add(MyDictionaryKey, 1);
+                                    newcube = true;
                                     try
                                     {
-                                        
-                                        //we delete old cubes first (append false)
-                                        StreamWriter outfile = new StreamWriter(MyCubeFullPath,false);
-                                        outfile.Write(Current);
-                                        
-                                        // Close the stream:
-                                        outfile.Close();
+                                        /*we start with an empty cube file first time
+                                         * we see a new cube */
+                                        if (File.Exists(MyCubeFullPath))
+                                        {
+                                            File.Delete(MyCubeFullPath);
+                                        }
                                     }
                                     catch (Exception ex)
                                     {
                                         Console.WriteLine("Error: " + ex.Message);
                                         return;
                                     }
+                                }
+
+
+                                //update the cube dictionary
+                                if (newcube == true)
+                                {
+                                    CubeDictionary.Add(MyDictionaryKey, 1);
                                     
+
                                 }
                                 else
                                 {
                                     CubeDictionary[MyDictionaryKey]++;
-                                    try
-                                    {
-                                        StreamWriter outfile = new StreamWriter(MyCubeFullPath, true);
-                                        outfile.Write(Current);
-
-                                        // Close the stream:
-                                        outfile.Close();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        Console.WriteLine("Error: " + ex.Message);
-                                        return;
-                                    }
                                     
+
+                                    
+                                    /*remember to flush buffer and write points to disk
+                                     * buffersize should go against total no. of points
+                                     * in CubeBufferDictionary and not number of points
+                                     * in each cube. this would force update some cubefiles
+                                     * early after few points but it gives a more true
+                                     * picture of the total memory used before flush*/
                                 }
-                                
+                                /*we keep things simple by deleting everything in
+                                 * the buffer dictionary, so we have to check separately
+                                 * if the key exist in the buffer */
+                                if (!CubeBufferDictionary.ContainsKey(MyDictionaryKey))
+                                {
+                                    List<string> BufferPointList = new List<string>();
+                                    BufferPointList.Add(Str);
+                                    CubeBufferDictionary.Add(MyDictionaryKey, BufferPointList);
+                                }
+                                else {
+                                    CubeBufferDictionary[MyDictionaryKey].Add(Str);
+                                }
+                                PointsInMemory++;
+                                TotalFilePointCounter++;
                                 TotalPointCounter++;
                             }
-                            // cubelengthx;
-
                         }
+                        #endregion
+                        #region debugpointlimitor
                         /*
-                        if (TotalPointCounter >= 100000)
+                        
+                        if (TotalPointCounter >= 10000)
                         {
-                            Console.WriteLine("debug safety stop  - stopping after 100000 points");
+                            Console.WriteLine("debug safety stop  - stopping after 10000 points");
+                            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey(true);
                             return;
                         }
-                         */
-                        
-    
-                        Str = SR.ReadLine();
+                        */
+                        /* Time elapsed: 00:01:13.9126781 (1min14s) version1
+                             * with stream open and close for each point
+                             * Time elapsed: 00:00:00.3980194 (0min0.4s) version2
+                             * enabled buffer size set to 1000
+                             */
+                        #endregion
+                    
+
+                        try
+                        {
+                            Str = MyInputStreamReader.ReadLine();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                            return;
+                        }
+
                     }
-                    foreach (KeyValuePair<string, ulong> kvp in CubeDictionary)
+                    if (MyInputStreamReader != null)
                     {
-                        Console.WriteLine("Cube: [" + kvp.Key + "] Points:" + kvp.Value);
+                        MyInputStreamReader.Close();
                     }
-                    Console.WriteLine("Number of cubes: " + CubeDictionary.Count);
-                    Console.WriteLine("Number of points in " + FileName + " : " + TotalPointCounter);
+                    if (MyInputFileStream != null)
+                    {
+                        MyInputFileStream.Close();
+                    }
+
+
+                    Console.WriteLine("Number of points in " + FileName + " : " + TotalFilePointCounter);
+
+                }
+                #endregion
+            }
+
+            //final buffer flush
+            if (PointsInMemory > 0)
+            {
+                Console.WriteLine("============================================================");
+                Console.WriteLine("Debug, flushing remaining PointsInMemory:" + PointsInMemory);
+                Console.WriteLine("============================================================");
+                string MyFlushError = string.Empty;
+                if (FlushBufferToDisk(DestinationFolder, CubeBufferDictionary, ref PointsInMemory,  out MyFlushError) == false)
+                {
+                    Console.WriteLine("Error," + MyFlushError);
+                    return;
                 }
             }
             
+            if (CubeDictionary.Count > 0)
+            {
+                foreach (KeyValuePair<string, ulong> kvp in CubeDictionary)
+                {
+                    Console.WriteLine("Cube: [" + kvp.Key + "] Points:" + kvp.Value);
+                }
+                Console.WriteLine("Number of cubes: " + CubeDictionary.Count);
+            }
+
             stopwatch.Stop();
+            Console.WriteLine("Number of points in total: " + TotalPointCounter);
             Console.WriteLine(System.Windows.Forms.Application.ProductName + " completed");
             Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
             //System.Threading.Thread.Sleep(2500);
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
+        }
+
+        private static bool FlushBufferToDisk(string DestinationFolder, Dictionary<string, List<string>> CubeBufferDictionary, ref int PointsInMemory,out string MyResultStr)
+        {
+            MyResultStr = string.Empty;
+            bool MyResultBool = true;
+            Console.WriteLine("Debug flush buffer");
+            //add code here to flush buffer to disk  
+            foreach (KeyValuePair<string, List<string>> kvp in CubeBufferDictionary)
+            {
+                Console.WriteLine("Debug key:" + kvp.Key);
+
+                //Console.WriteLine("Debug List to strings:" + item);
+
+                StreamWriter MyOutputStreamWriter = null;
+                string MyCubeFullPath = DestinationFolder + @"\" + kvp.Key + ".ptstemp";
+                Console.WriteLine("Debug output file: " + MyCubeFullPath);
+                try
+                {
+                    MyOutputStreamWriter = new StreamWriter(MyCubeFullPath, true);
+                    //StringBuilder sb = new StringBuilder();
+                    foreach (string item in kvp.Value)
+                    {
+                        //sb.AppendLine(item);
+                        MyOutputStreamWriter.WriteLine(item);
+                    }
+                    //MyOutputStreamWriter.Write(sb.ToString());
+                    MyOutputStreamWriter.Flush();
+                    
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    MyResultStr = ex.Message;
+                    MyResultBool = false;
+                }
+                finally
+                {
+                    if (MyOutputStreamWriter != null)
+                    {
+                        MyOutputStreamWriter.Close();
+                    }
+                }
+
+            }
+            CubeBufferDictionary.Clear();
+            PointsInMemory = 0;
+            return MyResultBool;
         }
 
         private static bool ClipSanityCheck(bool xlowerbool, int xlowerclip, bool xupperbool, int xupperclip, bool ylowerbool, int ylowerclip, bool yupperbool, int yupperclip, bool zlowerbool, int zlowerclip, bool zupperbool, int zupperclip)
